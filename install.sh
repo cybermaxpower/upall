d47bd6e6cecfb92c2cfdb9d1e18e44a4c1bd0708
Matt <1944402+cybermaxpower@users.noreply.github.com>
2026-06-12 14:21:15 +0100

    Initial commit: Add smart update script.

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Lazy instller.."
+echo "==============="
+echo " "
+echo "installing upall updatde script to system folder."
+echo " "
+sudo cp upall /usr/local/bin/upall
+sudo chmod +x /usr/local/bin/upall
+
